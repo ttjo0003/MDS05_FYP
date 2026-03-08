d4b389c2ceebf8724039f090f94b0a7caf962bf2
Nigel Tan <nigeltan501@gmail.com>
2026-03-08 13:52:18 +0800

    WC 1

---
+++ webcam.js
@@ -0,0 +1,17 @@
+class Main {
+
+    startWebcam() {
+    navigator.mediaDevices.getUserMedia({
+    video: {
+        facingMode: 'user'
+    },
+    audio: false
+    })
+    .then((stream) => {
+    this.video.srcObject = stream;
+    this.video.width = IMAGE_SIZE;
+    this.video.height = IMAGE_SIZE;
+    this.video.addEventListener('playing', () => this.videoPlaying = true);
+    this.video.addEventListener('paused', () => this.videoPlaying = false);
+    })
+}
